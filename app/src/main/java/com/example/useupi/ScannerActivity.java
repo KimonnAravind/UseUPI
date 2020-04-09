1c410dc538775569177ba39fd342a0a04e096eb0
kimonnaravind <aravindselvam03@gmail.com>
2020-04-09 15:11:32 +0530

    $ git commit -m "First commit"

--- app/src/main/java/com/example/useupi/ScannerActivity.java
+++ app/src/main/java/com/example/useupi/ScannerActivity.java
@@ -1,39 +1,117 @@
 package com.example.useupi;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.zxing.Result;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class ScannerActivity extends AppCompatActivity implements View.OnClickListener {
+import static android.Manifest.permission_group.CAMERA;
 
 
+public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int REQUEST_CAMERA = 123;
+    ZXingScannerView scannerView;
     private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scanner);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        qrScan = new IntentIntegrator(this);
-
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+        /*qrScan = new IntentIntegrator(this);
         qrScan.setOrientationLocked(false);
-        qrScan.initiateScan();
+        qrScan.initiateScan();*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M);
+        {
+
+            if(checkPermission())
+            {
+                if(scannerView==null)
+                {
+                    scannerView= new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            }
+            else
+            {
+                requestPermissions();
+            }
+
+
+        }
+
+    }
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
     }
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted) {
+                        Toast.makeText(ScannerActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(ScannerActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(ScannerActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA );
+                    }
+                }
+                break;
+        }
+    }
+
+    public void displayalert(String message, DialogInterface.OnClickListener listener) {
+        new AlertDialog.Builder(ScannerActivity.this).setMessage(message).setPositiveButton("OK", listener)
+                .setNegativeButton("CANCEL", null).create();
+    }
+
+
+    private Boolean checkPermission() {
+        return (ContextCompat.checkSelfPermission(ScannerActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        final String scanresult = rawResult.getText();
+        Toast.makeText(ScannerActivity.this, ""+scanresult, Toast.LENGTH_SHORT).show();
+
+    }
+    /*@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
@@ -58,5 +136,5 @@ public class ScannerActivity extends AppCompatActivity implements View.OnClickLi
     }
     @Override
     public void onClick(View view) {
-    }
+    }*/
 }
